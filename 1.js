925ae5c4cef7546fc1ce292c09362b465658a26d
Grinevskiy <last.avenger@yandex.ru>
2024-10-04 18:51:43 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,6 @@
+let n = 15
+let summ = 0;
+for(let i=1; i<(n+1); i++){
+    summ = summ + i;
+}
+console.log(summ);
---
+++ 2.js
@@ -0,0 +1,6 @@
+let n = 5
+let factorial = 1
+for(let i=1; i<(n+1); i++){
+    factorial *= i
+}
+console.log(factorial)
